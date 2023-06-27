bd6d5851235c9e060abd0efeeac4ae7f76eef48a
Matt MacFarlane <tidydata@outlook.com>
2023-06-27 10:30:13 -0400

    record audio option added, and removed api key b/c was being exposed and rotated

--- server/openai.py
+++ server/openai.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 import openai
+import sounddevice as sd
+import soundfile as sf
 
 # Load environment variables from .env file
 load_dotenv()
@@ -8,27 +10,47 @@ load_dotenv()
 # Set OpenAI API key from the environment variable
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+# Define the function for recording audio
+def record_audio(duration=5, sample_rate=44100):
+    print("Recording audio...")
+    audio = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=1)
+    sd.wait()
+    print("Audio recorded.")
+    return audio, sample_rate
+
+# Define the function for saving the recorded audio to a file
+def save_audio(audio, sample_rate, file_path):
+    sf.write(file_path, audio, sample_rate)
+
+# Set the file path for the MP3 file
 mp3_file = "/Users/mattmacfarlane/Development/code/phase-4b/voice-to-vision/server/test_audio/oklahoma.mp3"
-audio_file = open(f"{mp3_file}", "rb")
-transcript = openai.Audio.transcribe("whisper-1", audio_file) 
 
+# Define the file path for the recorded audio
+recorded_audio_file = "/Users/mattmacfarlane/Development/code/phase-4b/voice-to-vision/server/test_audio/recorded_audio.wav"
 
-# print(transcript) 
+# Check if the MP3 file exists
+if os.path.exists(mp3_file):
+    audio_file = open(mp3_file, "rb")
+else:
+    # Record audio if the MP3 file doesn't exist
+    audio, sample_rate = record_audio()
+    save_audio(audio, sample_rate, recorded_audio_file)
+    audio_file = open(recorded_audio_file, "rb")
+
+# Transcribe the audio to text using Whisper model
+transcript = openai.Audio.transcribe("whisper-1", audio_file)
+
+# Print the transcript
+print("Transcript:", transcript)
 
 # Text-to-Text
 
 response = openai.Completion.create(
     model="text-davinci-003",
-#   prompt=f"Create an ideal OpenAI DALLE-2 prompt based on themes and imagery pulled from the full text of: {transcript}, and not simply the beginning. The prompt should be concise, evocative, and encourage creative and imaginative visual interpretations. Consider using vivid language, sensory details, and emotional cues to inspire the Whisper model.",
-#    prompt=f"Create an ideal OpenAI DALLE-2 prompt to generate album art for the lyrics and themes and imagery in the full {transcript}",
-#    prompt=f"Transform lyrics into an ideal DALLE-2 or Stable Diffusion v2 prompt, for the purpose of generating images evokative of the lyrics. Use the full transcript, not just the beginning, in: {transcript}",
     prompt=f"Generate a visual prompt for OpenAI DALL-E 2 using the lyrics from: {transcript}",
     max_tokens=75,
     temperature=0
 )
-
-# to print JSON response: 
-# print(response) 
 
 choices = response['choices']
 if len(choices) > 0:
@@ -39,8 +61,8 @@ else:
 # Remove special characters
 dalle2prompt = ''.join(e for e in dalle2prompt if e.isalnum() or e.isspace())
 
-# prints only the dalle2prompt
-print(dalle2prompt)
+# Print only the dalle2prompt
+print("DALLE-2 Prompt:", dalle2prompt)
 
 # Text-to-Image
 
@@ -50,4 +72,5 @@ response = openai.Image.create(
     size="1024x1024"
 )
 
-print(response)
+# Print the response
+print("Image Response:", response)
