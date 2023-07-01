d100da9c6c4bd5441e8cdb1cadb84d52eb1794cc
Matt MacFarlane <tidydata@outlook.com>
2023-07-01 11:19:15 -0400

    navbar styling and more routes exposed

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 import axios from 'axios';
 import NavBar from "./NavBar";
 import Home from "./Home";
@@ -13,9 +15,22 @@ import Text2Images from "./Text2Images";
 import About from "./About";
 import Upload from "./Upload";
 import UploadOrRecordAudio from "./UploadOrRecordAudio";
-import "./App.css";
+import CssBaseline from "@mui/material/CssBaseline";
 
-
+// Create a custom theme
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#3f51b5',  // Custom primary color
+    },
+    secondary: {
+      main: '#f50057', // Custom secondary color
+    },
+  },
+  typography: {
+    fontFamily: 'Roboto',  // Font family (optional)
+  },
+});
 
 function App() {
   const [data, setData] = useState([]);
@@ -46,37 +61,125 @@ function App() {
   }
 
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route
-          path="/profile"
-          render={(props) => <Profile {...props} data={data} />}
-        />
-        <Route path = '/about' component = {About} />
-        <Route path = '/upload' component = {Upload} />
-        <Route path = '/form' component = {Form} />
-        <Route
-          path="/text2texts"
-          component={Text2Texts}
-        />
-        <Route
-          path="/audio2texts"
-          component={Audio2Texts}
-        />
-        
-        <Route
-          path="/text2images"
-          component={Text2Images}
-        />
-        <Route path="/uploadorrecordaudio" component={UploadOrRecordAudio} />
-        <Route path="/research_paper" component={ResearchPaper} />
-        <Route path="/imageresults" component={ImageResults} />
-        
-      </Switch>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <NavBar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route
+            path="/profile"
+            render={(props) => <Profile {...props} data={data} />}
+          />
+          <Route path = '/about' component = {About} />
+          <Route path = '/upload' component = {Upload} />
+          <Route path = '/form' component = {Form} />
+          <Route
+            path="/text2texts"
+            component={Text2Texts}
+          />
+          <Route
+            path="/audio2texts"
+            component={Audio2Texts}
+          />
+          <Route
+            path="/text2images"
+            component={Text2Images}
+          />
+          <Route path="/uploadorrecordaudio" component={UploadOrRecordAudio} />
+          <Route path="/research_paper" component={ResearchPaper} />
+          <Route path="/imageresults" component={ImageResults} />
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
 export default App;
+
+// import React, { useState, useEffect } from "react";
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+// import axios from 'axios';
+// import NavBar from "./NavBar";
+// import Home from "./Home";
+// import Profile from "./Profile";
+// import Text2Texts from "./Text2Texts";
+// import ImageResults from "./ImageResults";
+// import ResearchPaper from "./ResearchPaper.js";
+// import Form from "./Form";
+// import Audio2Texts from "./Audio2Texts";
+// import Text2Images from "./Text2Images";
+// import About from "./About";
+// import Upload from "./Upload";
+// import UploadOrRecordAudio from "./UploadOrRecordAudio";
+// import "./App.css";
+
+// const theme = createTheme();
+
+// function App() {
+//   const [data, setData] = useState([]);
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await axios.get("http://localhost:7000/audio2texts");
+//         setData(response.data);
+//         setIsLoading(false);
+//       } catch (error) {
+//         setError(error);
+//         setIsLoading(false);
+//       }
+//     };
+
+//     fetchData();
+//   }, []);
+
+//   if (isLoading) {
+//     return <div>Loading...</div>;
+//   }
+
+//   if (error) {
+//     return <div>Error: {error.message}</div>;
+//   }
+
+//   return (
+//     <ThemeProvider theme={theme}>
+//     <BrowserRouter>
+//       <NavBar />
+//       <Switch>
+//         <Route path="/" exact component={Home} />
+//         <Route
+//           path="/profile"
+//           render={(props) => <Profile {...props} data={data} />}
+//         />
+//         <Route path = '/about' component = {About} />
+//         <Route path = '/upload' component = {Upload} />
+//         <Route path = '/form' component = {Form} />
+//         <Route
+//           path="/text2texts"
+//           component={Text2Texts}
+//         />
+//         <Route
+//           path="/audio2texts"
+//           component={Audio2Texts}
+//         />
+        
+//         <Route
+//           path="/text2images"
+//           component={Text2Images}
+//         />
+//         <Route path="/uploadorrecordaudio" component={UploadOrRecordAudio} />
+//         <Route path="/research_paper" component={ResearchPaper} />
+//         <Route path="/imageresults" component={ImageResults} />
+        
+//       </Switch>
+//     </BrowserRouter>
+//     </ThemeProvider>
+//   );
+// }
+
+// export default App;
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,46 +1,63 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import './NavBar.css';
+import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
-    <nav className="navbar">
-      <h3 className="navbar-header">User-Related</h3>
-      <div className="navbar-item">
-        <NavLink to="/profile" activeClassName="navbar-link-active">Profile</NavLink>
-      </div>
-
-      <h3 className="navbar-header">Media-Related</h3>
-      <div className="navbar-item">
-        <NavLink to="/audio2texts" activeClassName="navbar-link-active">Audio to Text</NavLink>
-      </div>
-      <div className="navbar-item">
-        <NavLink to="/text2texts" activeClassName="navbar-link-active">Text to Text</NavLink>
-      </div>
-      <div className="navbar-item">
-        <NavLink to="/text2images" activeClassName="navbar-link-active">Text to Images</NavLink>
-      </div>
-      <div className="navbar-item">
-        <NavLink to="/upload" activeClassName="navbar-link-active">Upload MP3</NavLink>
-      </div>
-      <div className="navbar-item">
-        <NavLink to="/upload-or-record-audio" activeClassName="navbar-link-active">Upload or Record Audio</NavLink>
-      </div>
-
-      <h3 className="navbar-header">Miscellaneous</h3>
-      <div className="navbar-item">
-        <NavLink exact to="/" activeClassName="navbar-link-active">Home</NavLink>
-      </div>
-      <div className="navbar-item">
-        <NavLink to="/about" activeClassName="navbar-link-active">About</NavLink>
-      </div>
-      <div className="navbar-item">
-        <NavLink to="/form" activeClassName="navbar-link-active">Form</NavLink>
-      </div>
-      <div className="navbar-item">
-        <NavLink to="/research_paper" activeClassName="navbar-link-active">Research Paper</NavLink>
-      </div>
-    </nav>
+    <AppBar position="static" style={{ backgroundColor: 'rgba(0, 0, 255, 0.3)', boxShadow: 'none' }}>
+      <Toolbar>
+        <Box sx={{ display: 'flex', flexGrow: 1, justifyContent: 'space-between', alignItems: 'center' }}>
+          <Typography variant="h6" style={{ fontFamily: 'Arial, sans-serif' }}>
+            My Application
+          </Typography>
+          <Box sx={{ '& > :not(style)': { m: 1, textTransform: 'none', fontFamily: 'Verdana, sans-serif' } }}>
+            <Button variant="text" color="inherit" component={Link} to="/" 
+              sx={{ '&:hover': { backgroundColor: 'rgba(0, 0, 255, 0.2)' } }}>
+              Home
+            </Button>
+            <Button variant="text" color="inherit" component={Link} to="/profile"
+              sx={{ '&:hover': { backgroundColor: 'rgba(0, 0, 255, 0.2)' } }}>
+              Profile
+            </Button>
+            <Button variant="text" color="inherit" component={Link} to="/about"
+              sx={{ '&:hover': { backgroundColor: 'rgba(0, 0, 255, 0.2)' } }}>
+              About
+            </Button>
+            <Button variant="text" color="inherit" component={Link} to="/upload"
+              sx={{ '&:hover': { backgroundColor: 'rgba(0, 0, 255, 0.2)' } }}>
+              Upload
+            </Button>
+            <Button variant="text" color="inherit" component={Link} to="/form"
+              sx={{ '&:hover': { backgroundColor: 'rgba(0, 0, 255, 0.2)' } }}>
+              Form
+            </Button>
+            <Button variant="text" color="inherit" component={Link} to="/text2texts"
+              sx={{ '&:hover': { backgroundColor: 'rgba(0, 0, 255, 0.2)' } }}>
+              Text to Texts
+            </Button>
+            <Button variant="text" color="inherit" component={Link} to="/audio2texts"
+              sx={{ '&:hover': { backgroundColor: 'rgba(0, 0, 255, 0.2)' } }}>
+              Audio to Texts
+            </Button>
+            <Button variant="text" color="inherit" component={Link} to="/text2images"
+              sx={{ '&:hover': { backgroundColor: 'rgba(0, 0, 255, 0.2)' } }}>
+              Text to Images
+            </Button>
+            <Button variant="text" color="inherit" component={Link} to="/uploadorrecordaudio"
+              sx={{ '&:hover': { backgroundColor: 'rgba(0, 0, 255, 0.2)' } }}>
+              Upload or Record Audio
+            </Button>
+            <Button variant="text" color="inherit" component={Link} to="/research_paper"
+              sx={{ '&:hover': { backgroundColor: 'rgba(0, 0, 255, 0.2)' } }}>
+              Research Paper
+            </Button>
+            <Button variant="text" color="inherit" component={Link} to="/imageresults"
+              sx={{ '&:hover': { backgroundColor: 'rgba(0, 0, 255, 0.2)' } }}>
+              Image Results
+            </Button>
+          </Box>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 }
 
@@ -48,46 +65,45 @@ export default NavBar;
 
 // import React from 'react';
 // import { NavLink } from 'react-router-dom';
-// import './NavBar.css'; // Assuming you have a CSS file for styling your components
+// import './NavBar.css';
 
 // function NavBar() {
 //   return (
 //     <nav className="navbar">
+//       <h3 className="navbar-header">User-Related</h3>
 //       <div className="navbar-item">
-//         <NavLink exact to="/" activeClassName="navbar-link-active">Home</NavLink>
+//         <NavLink to="/profile" activeClassName="navbar-link-active">Profile</NavLink>
+//       </div>
+
+//       <h3 className="navbar-header">Media-Related</h3>
+//       <div className="navbar-item">
+//         <NavLink to="/audio2texts" activeClassName="navbar-link-active">Audio to Text</NavLink>
+//       </div>
+//       <div className="navbar-item">
+//         <NavLink to="/text2texts" activeClassName="navbar-link-active">Text to Text</NavLink>
+//       </div>
+//       <div className="navbar-item">
+//         <NavLink to="/text2images" activeClassName="navbar-link-active">Text to Images</NavLink>
 //       </div>
 //       <div className="navbar-item">
 //         <NavLink to="/upload" activeClassName="navbar-link-active">Upload MP3</NavLink>
 //       </div>
 //       <div className="navbar-item">
-//         <NavLink to="/audio2texts" activeClassName="navbar-link-active">Audio2Texts</NavLink>
+//         <NavLink to="/upload-or-record-audio" activeClassName="navbar-link-active">Upload or Record Audio</NavLink>
 //       </div>
+
+//       <h3 className="navbar-header">Miscellaneous</h3>
 //       <div className="navbar-item">
-//         <NavLink to="/text2texts" activeClassName="navbar-link-active">Text2Texts</NavLink>
-//       </div>
-//       <div className="navbar-item">
-//         <NavLink to="/text2images" activeClassName="navbar-link-active">Text2Images</NavLink>
-//       </div>
-//       <div className="navbar-item">
-//         <NavLink to="/profile" activeClassName="navbar-link-active">Profile</NavLink>
+//         <NavLink exact to="/" activeClassName="navbar-link-active">Home</NavLink>
 //       </div>
 //       <div className="navbar-item">
 //         <NavLink to="/about" activeClassName="navbar-link-active">About</NavLink>
 //       </div>
 //       <div className="navbar-item">
-//         <NavLink to="/logout" activeClassName="navbar-link-active">Logout</NavLink>
-//       </div>
-//       <div className="navbar-item">
-//         <NavLink to="/login" activeClassName="navbar-link-active">Login</NavLink>
-//       </div>
-//       <div className='navbar-item'>
-//         <NavLink to="/signup" activeClassName="navbar-link-active">Signup</NavLink>
-//       </div>
-//       <div className="navbar-item">
 //         <NavLink to="/form" activeClassName="navbar-link-active">Form</NavLink>
 //       </div>
 //       <div className="navbar-item">
-//         <NavLink to="/upload-or-record-audio" activeClassName="navbar-link-active">Upload or Record Audio</NavLink>
+//         <NavLink to="/research_paper" activeClassName="navbar-link-active">Research Paper</NavLink>
 //       </div>
 //     </nav>
 //   );
